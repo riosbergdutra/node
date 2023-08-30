1102f7384b97bc8f0d1f1d45bfbbe0d0e9402946
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-08-30 09:41:21 -0300

    unlink, rename, readfile e writefile

--- aula04/index.js
+++ aula04/index.js
@@ -1,7 +1,28 @@
 const fs = require("fs");
 
-fs.writeFile("./files/arquivo.txt", "esse é o meu primeiro arquivo", function(err){
-    if(err) {
-        return console.log(`algo deu errado ${err}`)
-    }
+// fs.writeFile("./files/arquivo.txt", "esse é o meu primeiro arquivo", function(err){
+//     if(err) {
+//         return console.log(`algo deu errado ${err}`)
+//     }
+// })
+
+// fs.readFile("./files/arquivo.txt","utf-8", function(err,data){
+//     if(err) {
+//         return console.log(`algo deu errado ${err}`)
+//     }
+
+//     console.log(data)
+// })
+
+// fs.rename("./files/arquivo.txt","./files/novo-nome.txt", function(err){
+//     if(err) {
+//         return console.log(`algo deu errado ${err}`)
+//     }
+//     console.log("renomeado com sucesso")
+// })
+
+fs.unlink("./files/novo-nome.txt", function(err){
+   if(err) {
+    return console.log (`algo deu errado ${err}`)
+   }
 })
