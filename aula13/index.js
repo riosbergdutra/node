6e0189f5fd6a5bc5a85fe308157972cde20543c2
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-08-31 16:18:54 -0300

    conhecendo o express

--- aula13/index.js
+++ aula13/index.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json())
+
+app.get("/", (req,res)=> {
+    res.send("nosso contato email@email.com")
+})
+
+app.listen(port, () => {
+    console.log(`servidor rodando em http://localhost:${port}`)
+})
