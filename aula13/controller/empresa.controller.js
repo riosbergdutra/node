95d4ee5079ec7142dadbe3c0684359873e955248
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-09-01 16:04:34 -0300

    Enviando e recebendo informações

--- aula13/controller/empresa.controller.js
+++ aula13/controller/empresa.controller.js
@@ -1,18 +1,41 @@
-const find = (req,res) => {
-    res.send("rota find")
+const empresas = [
+    {
+        id: 1,
+        nome: "empresa 1",
+        numFuncionarios: 100
+    },
+    {
+        id: 2,
+        nome: "empresa 2",
+        numFuncionarios: 1200
+    },
+    {
+        id: 3,
+        nome: "empresa 3",
+        numFuncionarios: 5000
+    },
+];
+
+const find = (req, res) => {
+    const id = req.params.id;
+    res.send(empresas.find(empresa => empresa.id === Number(id)));
 }
 
-
-const findAllEmpresa = (req,res) => {
-    res.send("Todas as empresas listadas")
+const findAllEmpresa = (req, res) => {
+    res.send(empresas);
 }
 
-const createEmpresa = (req,res) => {
-    res.send("empresa criada com sucesso")
+const createEmpresa = (req, res) => {
+    if(req.body.nome == null) {
+        return res.send ({menssage:"corpo da mensagem esta vazio"})
+    }
+    const novaEmpresa = req.body;
+    empresas.push(novaEmpresa);
+    res.send(empresas);
 }
 
 module.exports = {
     find,
     findAllEmpresa,
     createEmpresa
-}
+};
--- aula13/router/empresa.router.js
+++ aula13/router/empresa.router.js
@@ -2,6 +2,7 @@
 const router = require("express").Router()
 const empresa = require("../controller/empresa.controller");
 
+router.get("/find/:id", empresa.find)
 router.get("/findAll", empresa.findAllEmpresa);
 router.post("/create", empresa.createEmpresa)
 
