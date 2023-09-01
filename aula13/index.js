147133ca1ec2de3128885a41aa10288277a4f2b7
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-09-01 16:28:17 -0300

    Introdução às APIs Web

--- aula13/index.js
+++ aula13/index.js
@@ -1,20 +1,28 @@
 const express = require("express");
-const empresa = require("./router/empresa.router");
 const app = express();
+const request = require("request");
 
 
 const port = 4500;
 
 app.use(express.json())
 
-app.use("/empresa",empresa)
 
 app.get("/", (req,res)=> {
-    res.send("hello world")
-})
+    const url = req.body.url
 
-app.get("/contato", (req,res)=> {
-    res.send("nosso contato email@email.com")
+    request(url, function(error, response, body) {
+        console.log('statusCode:', response && response.statusCode)
+        const pokemon = JSON.parse(body)
+        res.send(pokemon)
+        
+        console.log(pokemon.id)
+        console.log(pokemon.name)
+        console.log(pokemon.height)
+        console.log(pokemon.weigth)
+        console.log(pokemon.types)
+
+    })
 })
 
 app.listen(port, () => {
